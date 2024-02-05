cbcf800e178043974d33149c14673f5322d2bd50
samsiegart <samsiegart@gmail.com>
2024-02-04 21:59:35 -0800

    scaffold create-cosmos-app

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Layout, Wallet } from "@/components";
+import { CHAIN_NAME } from "@/config";
+
+export default function Home() {
+  const [chainName, setChainName] = useState(CHAIN_NAME);
+
+  function onChainChange(chainName?: string) {
+    setChainName(chainName!);
+    console.log("current chain", chainName);
+  }
+
+  return (
+    <Layout>
+      <Wallet chainName={chainName} onChainChange={onChainChange} />
+    </Layout>
+  );
+}
